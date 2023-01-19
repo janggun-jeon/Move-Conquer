cf8d3d4412f89cd26f4c24932e986b91739186c7
janggun-jeon <man88821@gmail.com>
2023-01-19 21:49:20 +0900

    2023.01.19 21시
    씬 배치 미세조정
    게임 매니저에 클릭으로 이동 구현
    타이머 동기화

--- Assets/Timer.cs
+++ Assets/Timer.cs
@@ -5,13 +5,11 @@ using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
-    Text timer; // 타이머
+    Text timer;
 
     void Start()
     {
-        // 게임매니저 시스템 시간과 연동
         timer = gameObject.GetComponent<Text>();
-        timer.text = "Time : 30 sec";
     }
 
     void Update()
